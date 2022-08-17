4d0cb94f7f149a3c2243daed2af2d1c442de597f
Tais Rizzo <rizzotais@gmail.com>
2022-08-17 17:19:47 -0400

    ha

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,13 @@
+class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    @review.save
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,37 +5,37 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-puts "Cleaning database..."
+# puts 'Cleaning database...'
 # Restaurant.destroy_all
 
-puts "Creating restaurants..."
+puts 'Creating restaurants...'
 fiveguys = {
-  name: "Five Guys",
-  address: "Virginia Beach, 765, USA",
+  name: 'Five Guys',
+  address: 'Virginia Beach, 765, USA',
   phone_number: '634-9372',
   category: 'belgian'
 }
 papa_johns_pizza =  {
-  name: "Papa Johns Pizza",
-  address: "McLean 776, USA",
+  name: 'Papa Johns Pizza',
+  address: 'McLean 776, USA',
   phone_number: '634-9652',
   category: 'japanese'
 }
 cava =  {
-  name: "CAVA",
-  address: "FAirfax 876, USA",
+  name: 'CAVA',
+  address: 'Fairfax 876, USA',
   phone_number: '453-9372',
   category: 'french'
 }
 temakeria =  {
-  name: "Temakeria",
+  name: 'Temakeria',
   address: 'Arlington 55, USA',
   phone_number: '634-4535',
   category: 'chinese'
 }
 steak_house =  {
-  name: "Steak House",
-  address: "Washington DC 83, USA",
+  name: 'Steak House',
+  address: 'Washington DC 83, USA',
   phone_number: '634-3213',
   category: 'italian'
 }
@@ -44,4 +44,4 @@ steak_house =  {
   restaurant = Restaurant.create!(attributes)
   puts "Created #{restaurant.name}"
 end
-puts "Finished!"
+puts 'Finished!'
